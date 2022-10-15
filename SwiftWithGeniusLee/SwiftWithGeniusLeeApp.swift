bdbf3ed6e19f4525f167c527904328fb1ee96a19
TDCIAN <tdcian71@gmail.com>
2022-10-15 23:31:45 +0900

    modify project file

---
+++ SwiftWithGeniusLee/SwiftWithGeniusLeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWithGeniusLeeApp.swift
+//  SwiftWithGeniusLee
+//
+//  Created by JeongminKim on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWithGeniusLeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
