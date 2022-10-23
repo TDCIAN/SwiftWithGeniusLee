06a324ffbf2de09ada004f5f8d2c57b197a53afe
TDCIAN <tdcian71@gmail.com>
2022-10-23 14:42:46 +0900

    use removeDuplicates, compactMap, ignoreOutput, prefix

--- Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -6,17 +6,39 @@ import Combine
 var subscriptions = Set<AnyCancellable>()
 
 // removeDuplicates
+let words = "hey hey there! Mr Mr ?"
+    .components(separatedBy: " ")
+    .publisher
+
+words
+    .removeDuplicates()
+    .sink { value in
+        print("removeDuplicates() - received value - \(value)")
+    }.store(in: &subscriptions)
 
 
 // compactMap
+let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
 
+strings
+    .compactMap { Float($0) }
+    .sink { value in
+        print("compactMap - value: \(value)")
+    }.store(in: &subscriptions)
 
 
 // ignoreOutput
-
+let numbers = (1...10_000).publisher
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print("ignoreOutput() - Completed with: \($0)") }, receiveValue: { print("ignoreOutput() - Received Value: \($0)") }).store(in: &subscriptions)
 
 
 // prefix
+let tens = (1...10).publisher
+
+tens.prefix(2)
+    .sink(receiveCompletion: { print("prefix - Completed with: \($0)") }, receiveValue: { print("prefix - recevived value: \($0)")}).store(in: &subscriptions)
 
 
 
