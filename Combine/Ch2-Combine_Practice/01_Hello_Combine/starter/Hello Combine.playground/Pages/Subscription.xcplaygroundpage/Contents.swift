04f16f13f33c9f3374b6fc265ca79a657ee8bf2a
TDCIAN <tdcian71@gmail.com>
2022-10-23 13:31:39 +0900

    use subscription and published

--- Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,20 @@
 import Foundation
 import Combine
 
-
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[Debug]")
+    .sink { value in
+    print("Subscriber received value: \(value)")
+}
 
+subject.send("Hello")
+subject.send("Hello again!")
+subject.send("Hello for the last time!")
+//subject.send(completion: .finished) // 보내는 쪽에서 끝내는 거
+subscription.cancel() // 받는 쪽에서 끝내는 거
+subject.send("Hello?? :(")
 
 //: [Next](@next)
