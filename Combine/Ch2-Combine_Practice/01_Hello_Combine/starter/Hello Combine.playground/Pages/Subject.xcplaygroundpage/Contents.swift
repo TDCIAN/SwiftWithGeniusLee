f64566437acf34775999ac974be70827590358ec
TDCIAN <tdcian71@gmail.com>
2022-10-23 13:17:49 +0900

    use PassthroughSubject and CurrentValueSubject

--- Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -1,14 +1,29 @@
 import Foundation
 import Combine
 
-// PassthroughSubject
+// MARK: PassthroughSubject -> 바로 패싱해서 보냄
+let relay = PassthroughSubject<String, Never>()
+let subscription1 = relay.sink { value in
+    print("subscription1 received value: \(value)")
+}
+relay.send("Hello")
+relay.send("World!")
 
 
 
+// MARK: CurrentValueSubject -> Subscription이 새로 생겼을 때 기존에 가지고 있던 밸류를 한 번 보냄
+let variable = CurrentValueSubject<String, Never>("")
+print("first value: \(variable.value)")
+variable.send("Initial Value")
+print("seconde value: \(variable.value)")
+let subscription2 = variable.sink { value in
+    print("subscription2 received value: \(value)")
+}
+variable.send("More Text")
+print("third value: \(variable.value)")
 
-// CurrentValueSubject
-
-
-
+let publisher = ["Here", "we", "go"].publisher
+publisher.subscribe(relay)
+publisher.subscribe(variable)
 
 
