69105bf64615ea26e75cf685884e111b9d6aeb5f
TDCIAN <tdcian71@gmail.com>
2022-10-23 14:13:12 +0900

    use map and filter in combine

--- Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -4,9 +4,30 @@ import Foundation
 import Combine
 
 // Transform - Map
+let numPublisher = PassthroughSubject<Int, Never>()
+let subscription1 = numPublisher
+    .map { num -> String in
+        return "\(num)살입니다"
+    }
+    .sink { value in
+        print("Transformed Value: \(value)")
+    }
 
+numPublisher.send(10)
+numPublisher.send(20)
+numPublisher.send(30)
+subscription1.cancel()
 
 // Filter
+let stringPusblisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPusblisher
+    .filter { $0.contains("a") }
+    .sink { value in
+        print("Filtered Value: \(value)")
+    }
 
-
+stringPusblisher.send("Amen")
+stringPusblisher.send("amen")
+stringPusblisher.send("Bucks")
+stringPusblisher.send("Check")
 //: [Next](@next)
